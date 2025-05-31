0cd6de68a6adc6262e24d5895f7e36dc0aaafa1e
Sophia <sophia.soares.silva@gmail.com>
2025-05-31 17:35:17 +0200

    feat: create Analyzer interface

---
+++ src/main/java/de/exxcellent/challenge/analyzer/Analyzer.java
@@ -0,0 +1,17 @@
+package de.exxcellent.challenge.analyzer;
+
+import de.exxcellent.challenge.model.Table;
+
+/**
+ * Interface for analyzing data
+ */
+public interface Analyzer {
+    /**
+     * Returns the index of the row with the minimum spread.
+     * @param table The table to be analyzed
+     * @param columnName1 The name of the first column
+     * @param columnName2 The name of the second column
+     * @return The index of the row with the minimum spread
+     */
+    int getMinimumSpreadIndex(Table table, String columnName1, String columnName2);
+}
