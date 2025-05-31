292eec19faf2b8bd2fae2787ca9732bb6cc2cfe5
Sophia <sophia.soares.silva@gmail.com>
2025-05-31 19:33:15 +0200

    feat: implement football analyzer and integrate it in main function

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,6 +1,7 @@
 package de.exxcellent.challenge;
 
 import de.exxcellent.challenge.analyzer.WeatherAnalyzer;
+import de.exxcellent.challenge.analyzer.FootballAnalyzer;
 import de.exxcellent.challenge.model.Table;
 import de.exxcellent.challenge.util.CsvReader;
 import de.exxcellent.challenge.util.Reader;
@@ -20,11 +21,18 @@ public final class App {
     public static void main(String... args) {
         Reader reader = new CsvReader();
         WeatherAnalyzer weatherAnalyzer = new WeatherAnalyzer();
+        FootballAnalyzer footballAnalyzer = new FootballAnalyzer();
         
         // Analyze weather data
         Table weatherTable = reader.readData("de/exxcellent/challenge/weather.csv");
         int dayIndex = weatherAnalyzer.getMinimumSpreadIndex(weatherTable, "MxT", "MnT");
         String dayWithSmallestTempSpread = (String) weatherTable.getData()[dayIndex][0]; // Day is first column
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+    
+        // Analyze football data
+        Table footballTable = reader.readData("de/exxcellent/challenge/football.csv");
+        int teamIndex = footballAnalyzer.getMinimumSpreadIndex(footballTable, "Goals", "Goals Allowed");
+        String teamWithSmallestGoalSpread = (String) footballTable.getData()[teamIndex][0]; // Team is first column
+        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
