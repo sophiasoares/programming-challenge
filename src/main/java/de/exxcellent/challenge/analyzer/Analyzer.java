611191bcd4b10721776e2dfd56a32560e2176851
Sophia <sophia.soares.silva@gmail.com>
2025-05-31 23:03:31 +0200

    refactor: change interface Analyzer to an abstract class

--- src/main/java/de/exxcellent/challenge/analyzer/Analyzer.java
+++ src/main/java/de/exxcellent/challenge/analyzer/Analyzer.java
@@ -3,9 +3,9 @@ package de.exxcellent.challenge.analyzer;
 import de.exxcellent.challenge.model.Table;
 
 /**
- * Interface for analyzing data
+ * Abstract class for analyzing data with common functionality
  */
-public interface Analyzer {
+public abstract class Analyzer {
     /**
      * Returns the index of the row with the minimum spread.
      * @param table The table to be analyzed
@@ -13,5 +13,45 @@ public interface Analyzer {
      * @param col2Name The name of the second column
      * @return The index of the row with the minimum spread
      */
-    int getMinimumSpreadIndex(Table table, String col1Name, String col2Name);
+    public int getMinimumSpreadIndex(Table table, String col1Name, String col2Name) {
+        if (table == null) {
+            throw new IllegalArgumentException("Table cannot be null");
+        }
+        
+        Object[][] data = table.getData();
+        
+        // Get column indices
+        int col1Index = table.getColumnIndex(col1Name);
+        int col2Index = table.getColumnIndex(col2Name);
+        
+        if (col1Index == -1 || col2Index == -1) {
+            throw new IllegalArgumentException("Column not found: " + col1Name + " or " + col2Name);
+        }
+        
+        // Find minimum spread
+        double minSpread = Double.MAX_VALUE;
+        int minSpreadIndex = -1;
+        
+        for (int i = 0; i < data.length; i++) {
+            try {
+                double val1 = Double.parseDouble((String) data[i][col1Index]);
+                double val2 = Double.parseDouble((String) data[i][col2Index]);
+                double spread = Math.abs(val1 - val2);
+                
+                if (spread < minSpread) {
+                    minSpread = spread;
+                    minSpreadIndex = i;
+                }
+            } catch (NumberFormatException e) {
+                // Skip rows with invalid numbers
+                System.err.println("Could not parse value in row " + i);
+            }
+        }
+        
+        if (minSpreadIndex == -1) {
+            throw new IllegalArgumentException("No valid data found for spread calculation");
+        }
+        
+        return minSpreadIndex;
+    }
 }
